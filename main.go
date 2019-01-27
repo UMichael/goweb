905353ddc969141dcc8f179c3c49461a489857b2
michael <michael@localhost.localdomain>
2019-01-28 00:01:35 +0100

    done

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	login "./Handlers"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = os.Getenv("HTTP_PLATFORM_PORT")
+		if port == "" {
+			port = "8080"
+		}
+	}
 	var person login.User
 	//fileServer := http.FileServer(http.Dir("./template/images"))
 	router := httprouter.New()
